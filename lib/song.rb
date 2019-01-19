f23fd6204f34bf74898d9f0b0a57057f184a09b9
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-19 19:31:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,18 @@
 class Song
   attr_accessor :name, :artist
   @@all = Array.new
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def artist_name
     self.artist.nil? ? nil : self.artist.name
   end
+
+  def self.all
+    @@all
+  end
 end
+
