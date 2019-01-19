60373e6fd3022d4d1ce9aaa01358f0ac97549020
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-19 19:25:12 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,14 @@
 class Post
   attr_accessor :title, :author
+  @@all = Array.new
 
   def initialize(title)
     @title = title
     @@all << title
+  end
+
+  def self.all
+    @@all
   end
 
 end
